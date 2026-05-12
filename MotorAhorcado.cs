fc48d8008b7272b0812f6d14140fabae942a2bbf
Crisdan1505 <cristopher.euan@tecdesoftware.edu.mx>
2026-05-12 13:12:20 -0600

    REFACTOR:Correción de codgigo en la clase MotorAhorcado, codigo duplicado.

--- MotorAhorcado.cs
+++ MotorAhorcado.cs
@@ -5,52 +5,28 @@
         private readonly string _palabraSecreta;
         private readonly List<char> _letrasUsadas = new();
         private int _intentosRestantes = 6;
-
         public string PalabraSecreta => _palabraSecreta;
         public List<char> LetrasUsadas => _letrasUsadas;
         public int IntentosRestantes => _intentosRestantes;
-
         public MotorAhorcado(IRepositorioPalabras repositorio)
         {
             _palabraSecreta = repositorio.ObtenerPalabraAleatoria();
         }
 
-        public bool LetraYaUsada(char letra)
-        {
-            return _letrasUsadas.Contains(letra);
-        }
-
-        public bool EsLetraCorrecta(char letra)
-        {
-            return _palabraSecreta.Contains(letra);
-        }
-
+        public bool LetraYaUsada(char letra) => _letrasUsadas.Contains(letra);
+        public bool EsLetraCorrecta(char letra) => _palabraSecreta.Contains(letra);
         public void RegistrarLetra(char letra)
         {
             _letrasUsadas.Add(letra);
-
             if (!_palabraSecreta.Contains(letra))
-            {
                 _intentosRestantes--;
-            }
         }
-
         public bool Ganado()
         {
             foreach (char c in _palabraSecreta)
-            {
-                if (!_letrasUsadas.Contains(c))
-                {
-                    return false;
-                }
-            }
-
+                if (!_letrasUsadas.Contains(c)) return false;
             return true;
         }
-
-        public bool Perdido()
-        {
-            return _intentosRestantes <= 0;
-        }
+        public bool Perdido() => _intentosRestantes <= 0;
     }
 }
